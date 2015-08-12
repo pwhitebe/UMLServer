8b406f25d016d7a2d04c89a5f1948b5a7e87ed95
Sanjith David <sdavid@deloitte.com>
2015-08-12 14:39:06 -0400

    Disabled passport for now

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -1,10 +1,10 @@
 var express = require('express'),
 csv = require('express-csv'),
-	cookieParser = require('cookie-parser'),
-	session = require('express-session')
-	passport = require('passport');
-var morgan = require('morgan');
-var fs = require('fs')
+	cookieParser = require('cookie-parser');
+// 	session = require('express-session')
+// 	passport = require('passport');
+// var morgan = require('morgan');
+// var fs = require('fs')
 var properties = require('../lib/envProperties');
 
 //var accessLogStream = fs.createWriteStream(properties.ACCESS_LOG, {flags: 'a'})
@@ -16,11 +16,11 @@ module.exports = function(app, config) {
         app.set('view engine', 'jade');
         app.use(cookieParser());
         app.use(express.bodyParser());
-        app.use(session({secret:'use the force',resave:false,saveUninitialized:false}));
-        app.use(passport.initialize());
-        app.use(passport.session());
+        // app.use(session({secret:'use the force',resave:false,saveUninitialized:false}));
+        // app.use(passport.initialize());
+        // app.use(passport.session());
         //app.use(morgan('combined', {stream: accessLogStream}));
-        app.use(morgan('dev')); //LOCAL LOGGING
+        //app.use(morgan('dev')); //LOCAL LOGGING
 
         
     });
