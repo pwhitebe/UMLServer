7f9272ce7f6caf9508c735496614779f9700987f
Michael Ta <technical.ta@gmail.com>
2015-08-18 11:00:42 -0400

    database connection name to match our schema

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'mmwrcasewebapp',
   password : 'mmwrquizcase',
-  database : 'mmwrcase',
+  database : 'mmwr_case',
   port     : '/tmp/mysql.sock' 
 });
 connection.connect();
