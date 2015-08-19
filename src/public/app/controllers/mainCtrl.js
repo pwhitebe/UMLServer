2129f5538da90fc9060968fe8faf4d8f51ef961e
Michael Ta <technical.ta@gmail.com>
2015-08-19 09:32:28 -0400

    update img folder with correct assets

--- src/public/app/controllers/mainCtrl.js
+++ src/public/app/controllers/mainCtrl.js
@@ -1,13 +1,12 @@
-angular.module('app').controller('mainCtrl', function($scope,$http) {
+angular.module('app').controller('mainCtrl', function($scope, $http) {
 
-
-	$http.get('/api/mmwrcase/currentCase/').then(function(res){
-       if(res.data) {
-           $scope.currentCase = res.data[0];
-           console.log($scope.currentCase);
-           } else {
-               alert('no data received');
-           }
-      });
+	$http.get('/api/mmwrcase/currentCase/').then(function(res) {
+		if (res.data) {
+			$scope.currentCase = res.data[0];
+			console.log($scope.currentCase);
+		} else {
+			alert('no data received');
+		}
+	});
 
 });
