ba1e628f4c6da809708f8ba0463b55396bb150c1
Michael Ta <technical.ta@gmail.com>
2015-08-20 14:08:48 -0400

    pull previous case data from db

--- src/public/app/controllers/home/previousCaseCtrl.js
+++ src/public/app/controllers/home/previousCaseCtrl.js
@@ -1,3 +1,14 @@
 angular.module('app').controller('previousCaseCtrl', function($scope, $http) {
+	$scope.displayStatus = 2;
+	$scope.developmentStatus = 5;
 
-})
+	$http.get('/api/mmwrcase/getCasesByStatus/' + $scope.developmentStatus + '/' + $scope.displayStatus).then(function(res) {
+		if (res.data) {
+			$scope.previousCases = res.data;
+			console.log($scope.previousCases);
+		} else {
+			alert('no data received');
+		}
+	});
+
+});
