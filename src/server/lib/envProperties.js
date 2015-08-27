09614f60a501ac0f8bd93e0793ceab46e9c8bf24
Michael Ta <technical.ta@gmail.com>
2015-08-27 11:48:24 -0400

    adjusted config to use envProperties

--- src/server/lib/envProperties.js
+++ src/server/lib/envProperties.js
@@ -1,11 +1,15 @@
 //LOCAL
 
 module.exports = {
-  MONGO_DOMAIN: 'mongodb://localhost:27017/mmwrcase',
+  
+  mysqlhost: '127.0.0.1',
+  mysqluser: 'mmwrcasewebapp',
+  mysqlpassword: 'mmwrquizcase',
+  mysqldatabase: 'mmwr_case'
 
-  FACEBOOK_ID:      '1429109474024840',
-  FACEBOOK_SECRET:  '71ce33ce1aaa2da3d859d78a3848c164',
-  FACEBOOK_CALLBACK: 'http://localhost:8089/apollo/auth/facebook/callback',
+  // FACEBOOK_ID:      '1429109474024840',
+  // FACEBOOK_SECRET:  '71ce33ce1aaa2da3d859d78a3848c164',
+  // FACEBOOK_CALLBACK: 'http://localhost:8089/apollo/auth/facebook/callback',
 
   //ACCESS_LOG: '/apollologs/access_log'
 
