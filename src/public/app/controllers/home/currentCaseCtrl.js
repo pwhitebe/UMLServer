e72ce241595afae9f0e7d75bca09e7241a68bb89
Michael Ta <technical.ta@gmail.com>
2015-09-11 09:17:35 -0400

    dynamically bring in featured image

--- src/public/app/controllers/home/currentCaseCtrl.js
+++ src/public/app/controllers/home/currentCaseCtrl.js
@@ -14,6 +14,11 @@ angular.module('app').controller('currentCaseCtrl', function($scope, ngCase) {
 			.success(function(currentCase){
 				$scope.currentCase = currentCase;
 				//console.log($scope.currentCase);
+				for(var i =0; i < $scope.currentCase.images.length; i++) {
+					if($scope.currentCase.images[i].featured === 1) {
+						$scope.featuredImageUrl = $scope.currentCase.images[i].image_url;
+					}
+				}
 			})
 			.error(function(err) {
 				console.log('Case data unvailable');
