efd78794346098e5f9d9fee9ee27c08b621e6b94
missilen <tnmissilen@gmail.com>
2015-09-17 15:27:59 -0400

    implement pre and post question editing

--- src/public/app/controllers/authoring/editCaseCtrl.js
+++ src/public/app/controllers/authoring/editCaseCtrl.js
@@ -32,8 +32,10 @@ $scope.case = {
 	"images" : []
 }
 
-$scope.qa = [];
-
+$scope.qa = {
+	'pre' :[],
+	'post' : []
+}
 $scope.images = [];
 
 getDisplayStatus();
@@ -47,15 +49,15 @@ function getCaseData() {
 	ngCase.getCaseById($stateParams.caseID)
 			.success(function(caseData){
 				$scope.case = caseData;
-				console.log($scope.case);
 				if ($scope.case.QA.length > 0) {
 					// the case has some questions
 					for (var i = 0; i < $scope.case.QA.length; i++) {
-						var oneQA = {
-							'question' 	: $scope.case.QA[i].question,
-							'answers'	: $scope.case.QA[i].answers 
-						}
-						$scope.qa.push(oneQA)
+							var oneQA = {
+								'question' 	: $scope.case.QA[i].question,
+								'answers'	: $scope.case.QA[i].answers 
+							}
+						$scope.qa[$scope.case.QA[i].question.post_pre].push(oneQA);
+						
 					}
 				}
 			})
@@ -95,142 +97,67 @@ $scope.publicationDateChanged = function (newDate, oldDate) {
 }
 
 
-$scope.addTopic = function(qa,e) {
-    if (!qa.newQuestion || qa.newQuestion.length === 0) {
-      return;
-    }
-    // if ($scope.eventdoc.categories.indexOf(category).topics.length > 10) {
-    //   window.alert('You can\'t add more than 10 topics!');
-    //   return;
-    // }
-    //var topicName = document.getElementById('topicName').value;
-    var questionText = qa.newQuestion;
-    if (questionText.length > 0) {
-      qa.questions.push({
-        question: questionText,
-        type: 'topic',
-        answers: [],
-        sortOrder: qa.questions.length
-      });
-
-    }
-    qa.newQuestion ="";
-    e.preventDefault();
-  };
-
-  $scope.editTopic = function(topic) {
-    topic.editing = true;
-  };
-
-  $scope.cancelEditingTopic = function(topic) {
-    topic.editing = false;
-  };
-
-  $scope.saveTopic = function(topic,e) {
-    // topic.save();
-    topic.editing = false;
-    e.preventDefault();
-  };
-
-  $scope.removeTopic = function(qa, question) {
-    // if (window.confirm('Are you sure to remove this topic?')) {
-    //   //topic.destroy();  //TODO
-    // }
-    var index = qa.questions.indexOf(question);
-    if (index > -1) {
-      qa.questions.splice(index, 1)[0];
-    }
-  };
-
-  $scope.saveTopics = function() {
-    for (var i = $scope.eventdoc.categories[0].topics.length - 1; i >= 0; i--) {
-      var topic = $scope.eventdoc.categories[0].topics[i];
-      topic.sortOrder = i + 1;
-      // topic.save();
-    }
-  };
-
-  $scope.addSubTopic = function(question,e) {
-    
-    if (!question.newAnswer || question.newAnswer.length === 0) {
-      return;
-    }
-    question.answers.push({
-      answer: question.newAnswer,
-      sortOrder: question.answers.length,
-      type: 'subTopic',
-    });
-    question.newAnswer = '';
-    e.preventDefault();
-    // topic.save();
-  };
-
-  $scope.removeSubTopic = function(topic, subTopic) {
-    //if (window.confirm('Are you sure to remove this subTopic?')) {
-    var index = topic.subTopics.indexOf(subTopic);
-    if (index > -1) {
-      topic.subTopics.splice(index, 1)[0];
-    }
-    // topic.save();
-    //}
-  };
-
-  $scope.editSubTopic = function(subTopic) {
-    $log.debug("edit sub topic",subTopic);
-    subTopic.editing = true;
-  };
-
-  $scope.saveSubTopic = function(subTopic,e) {
-    // topic.save();
-    subTopic.editing = false;
-    e.preventDefault();
-  };
-
-  $scope.options = {
-    accept: function(sourceNode, destNodes, destIndex) {
-      var data = sourceNode.$modelValue;
-      var destType = destNodes.$element.attr('data-type');
-      return (data.type == destType); // only accept the same type
-    },
-    dropped: function(event) {
-      $log.debug(event);
-      var sourceNode = event.source.nodeScope;
-      var destNodes = event.dest.nodesScope;
-      // update changes to server
-      if (destNodes.isParent(sourceNode) && destNodes.$element.attr('data-type') == 'subTopic') { // If it moves in the same topic, then only update topic
-        var topic = destNodes.$nodeScope.$modelValue;
-        // topic.save();
-      } else { // save all
-        $scope.saveTopics();
-      }
-    },
-    beforeDrop: function(event) {
-      // if (!window.confirm('Are you sure you want to drop it here?')) {
-      //   event.source.nodeScope.$$apply = false;
-      // }
-    }
-  };
-
-
-// modified section
-
-$scope.addQuestion = function(qa,e) {
-    if (!qa.newQuestion || qa.newQuestion.length === 0) {
+$scope.addQuestion = function(qa,type,e) {
+    if (!qa.newQuestionText || qa.newQuestionText.length === 0) {
       return;
     }
    
-    var questionText = qa.newQuestion;
-    if ( questionText.length > 0) {
-      qa.push({
-        question: questionText,
-        type: 'question',
-        answers: [],
-        sortOrder: qa.length
-      });
-
+    var questionText = qa.newQuestionText;
+    if (questionText.length > 0) {
+    	// construct a question object before pushing
+    	var newQA = {
+    		'question' : {
+		    		'question_id': $scope.qa.length,
+		    		'case_id' : $scope.case.case_id || null,
+		    		'post_pre': type,
+		    		'question': questionText
+    		},
+    		answers : []
+    	}
+    //  console.log(newQuestion);
+      qa.push(newQA);
     }
-    qa.newQuestion ="";
+    qa.newQuestionText ="";
     e.preventDefault();
+  };
+
+ 
+  $scope.removeQuestion = function(qa, question) {
+    var index = qa.indexOf(question);
+    if (index > -1) {
+      qa.splice(index, 1)[0];
+    }
+  };
+
+  
+
+  $scope.addAnswer = function(question,e) {
+    
+    if (!question.newAnswerText || question.newAnswerText.length === 0) {
+      return;
+    }
+    // contruct answer object here
+    var newAnswer = {
+    	'answer_id' :  question.answers.length,
+    	'question_id'	: question.question.question_id,
+       	'case_id': $scope.case.case_id,
+    	'answer' : question.newAnswerText,
+    	'correct': 0,
+    	'hit_counter' : 0
+    }
+    question.answers.push(newAnswer);
+    question.newAnswerText = '';
+    e.preventDefault();
+    // topic.save();
+  };
+
+  $scope.removeAnswer = function(question, answer) {
+    var index = question.answers.indexOf(answer);
+    if (index > -1) {
+      question.answers.splice(index, 1)[0];
+    }
+    // topic.save();
+    //}
   };
 
   $scope.editQuestion = function(question) {
@@ -247,49 +174,16 @@ $scope.addQuestion = function(qa,e) {
     e.preventDefault();
   };
 
-  $scope.removeQuestion = function(qa,question) {
-    // if (window.confirm('Are you sure to remove thisquestion?')) {
-    //   //Question.destroy();  //TODO
-    // }
-    $log.debug(question);
-    var index = qa.questions.indexOf(question);
-    if (index > -1) {
-      qa.questions.splice(index, 1)[0];
-    }
-  };
-
+  
   $scope.saveQuestions = function() {
-    for (var i = $scope.qa.questions.length - 1; i >= 0; i--) {
-      var question = $scope.qa.questions[i];
+    for (var i = $scope.qa.length - 1; i >= 0; i--) {
+      var question = $scope.qa[i];
      question.sortOrder = i + 1;
       //question.save();
     }
   };
 
-  $scope.addAnswer = function(question,e) {
-    if (!question.newAnswer || question.newAnswer.length === 0) {
-      return;
-    }
-   question.answers.push({
-      answer:question.newAnswer,
-      sortOrder:question.answers.length,
-      type: 'answer'
-    });
-   question.newAnswer = '';
-    e.preventDefault();
-    //question.save();
-  };
-
-  $scope.removeAnswer = function(question, answer) {
-    //if (window.confirm('Are you sure to remove this answer?')) {
-    var index =question.answers.indexOf(answer);
-    if (index > -1) {
-     question.answers.splice(index, 1)[0];
-    }
-    //question.save();
-    //}
-  };
-
+  
   $scope.editAnswer = function(answer) {
     $log.debug("edit subquestion",answer);
     answer.editing = true;
@@ -301,24 +195,24 @@ $scope.addQuestion = function(qa,e) {
     e.preventDefault();
   };
 
-  $scope.options = {
+$scope.options = {
     accept: function(sourceNode, destNodes, destIndex) {
       var data = sourceNode.$modelValue;
-      var destType = destNodes.$element.attr('data-type');
-      return (data.type == destType); // only accept the same type
+      if (data.hasOwnProperty('question')) {
+         sourceType = 'question';
+      } else if (data.hasOwnProperty('answer')) {
+         sourceType = 'answer';
+      }
+
+     var destType = destNodes.$element.attr('data-type');
+      return (sourceType == destType); // only accept the same type
     },
     dropped: function(event) {
       $log.debug(event);
       var sourceNode = event.source.nodeScope;
       var destNodes = event.dest.nodesScope;
       // update changes to server
-      if (destNodes.isParent(sourceNode) && destNodes.$element.attr('data-type') == 'answer') { // If it moves in the samequestion, then only updatequestion
-        varquestion = destNodes.$nodeScope.$modelValue;
-        //question.save();
-      } else { // save all
-        $scope.saveQuestions();
-      }
-    },
+   },
     beforeDrop: function(event) {
       // if (!window.confirm('Are you sure you want to drop it here?')) {
       //   event.source.nodeScope.$$apply = false;
@@ -333,6 +227,20 @@ $scope.saveCase = function(editMode) {
 	else if (editMode == 'new') {
 		// save case
 	}
+}
+
+$scope.byQuestionType = function(type) {
+	return function(question) {
+		//console.log(question);
+        return question.question.post_pre == type;
+    }
+}
+
+$scope.setCorrectAnswer = function(question,answer) {
+	for (var i=0; i < question.answers.length; i++) {
+		question.answers[i].correct = 0;
+	}
+	answer.correct = 1;
 }
 
 });
