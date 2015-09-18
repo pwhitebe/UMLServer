f3b06f15fc911e393d2bbc04fa23e8ed80bfc55b
Michael Ta <technical.ta@gmail.com>
2015-09-18 09:09:10 -0400

    passport foundation

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -1,8 +1,8 @@
 var express = require('express'),
-csv = require('express-csv'),
-	cookieParser = require('cookie-parser');
-// 	session = require('express-session')
-// 	passport = require('passport');
+    csv = require('express-csv'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	passport = require('passport');
 // var morgan = require('morgan');
 // var fs = require('fs')
 var properties = require('../lib/envProperties');
@@ -16,9 +16,9 @@ module.exports = function(app, config) {
         app.set('view engine', 'jade');
         app.use(cookieParser());
         app.use(express.bodyParser());
-        // app.use(session({secret:'use the force',resave:false,saveUninitialized:false}));
-        // app.use(passport.initialize());
-        // app.use(passport.session());
+        app.use(session({secret:'use the force',resave:false,saveUninitialized:false}));
+        app.use(passport.initialize());
+        app.use(passport.session());
         //app.use(morgan('combined', {stream: accessLogStream}));
         //app.use(morgan('dev')); //LOCAL LOGGING
 
