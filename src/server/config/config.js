b5337d2527835b7273544c43321a47684c07ce84
Michael Ta <technical.ta@gmail.com>
2015-09-21 11:39:15 -0400

    create dbConnection module and used in current calls to DB

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,25 +1,9 @@
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
-var properties = require('../lib/envProperties');
 
 module.exports = {
 	//db: properties.MONGO_DOMAIN,
     rootPath: rootPath
 };
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : properties.mysqlhost,
-  user     : properties.mysqluser,
-  password : properties.mysqlpassword,
-  database : properties.mysqldatabase
-  //port     : '/tmp/mysql.sock' 
-});
-connection.connect( function(err) {
-	if(err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
 
-	console.log('connected to mysql db as id ' + connection.threadId);
-});
