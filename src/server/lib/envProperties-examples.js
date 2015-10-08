f68c1a9294f87aa68a02623f7350e1c866580d24
Peter White <pwhitebe@gmail.com>
2015-10-08 11:02:27 -0400

    Removing defunct password

--- src/server/lib/envProperties-examples.js
+++ src/server/lib/envProperties-examples.js
@@ -2,15 +2,9 @@
 
 module.exports = {
   
-  mysqlhost: '127.0.0.1',
-  mysqluser: 'mmwrcasewebapp',
-  mysqlpassword: 'mmwrquizcase',
-  mysqldatabase: 'mmwr_case'
-
-  // FACEBOOK_ID:      '1429109474024840',
-  // FACEBOOK_SECRET:  '71ce33ce1aaa2da3d859d78a3848c164',
-  // FACEBOOK_CALLBACK: 'http://localhost:8089/apollo/auth/facebook/callback',
-
-  //ACCESS_LOG: '/apollologs/access_log'
+  mysqlhost: '[mysql-host]',
+  mysqluser: '[mysql-user]',
+  mysqlpassword: '[mysql-password]',
+  mysqldatabase: '[mysql-database]'
 
 };
